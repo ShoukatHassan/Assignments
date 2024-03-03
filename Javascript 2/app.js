08fb6e6e7f20c5706c81ff703038ca7355214d65
ShoukatHassan <154282686+ShoukatHassan@users.noreply.github.com>
2024-03-04 02:17:11 +0500

    Add files via upload

---
+++ Javascript 2/app.js
@@ -0,0 +1,46 @@
+// Q no-1
+var userName = "Shoukat-01";
+console.log(userName)
+
+
+// Q no-2
+var myName = "Shoukat Hassan";
+console.log(myName)
+
+// Q no-3
+var message = "Hello World";
+alert(message)
+
+// Q no-4
+var name1 = "Shoukat Hassan";
+alert(name1)
+
+var age ="29";
+alert(age)
+
+var lineOfWork = "Certified Web & Mobile App Developer";
+alert(lineOfWork)
+
+
+// Q no-5
+var lineBreak = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+alert(lineBreak)
+
+
+// Q no-6
+var email = " My Email = www.Shoukatkhan.429@gmail.com"
+alert(email)
+
+
+// Q no-7
+var book = "i am trying to learn from the Book \n A Smart Way To Learn JAVA SCRIPT"
+alert(book)
+
+// Q no-8
+var content = "Yeah! we can write HTML content through JAVASCRIPT"
+document.write(content)
+ 
+// Q no-9
+
+var end = " “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” "
+alert(end)
