72a33674e547ea6b9e8022646b5318e28ffb438e
ShoukatHassan <154282686+ShoukatHassan@users.noreply.github.com>
2024-03-04 02:18:13 +0500

    Add files via upload

---
+++ Javascript 3/app.js
@@ -0,0 +1,24 @@
+// Q no-1
+
+var age = "i am 29 years old"
+alert(age)
+
+// Q no-2
+
+var websiteVisitors = "You have visited this site 10 times "
+document.write(websiteVisitors); 
+
+// Q no-3
+
+var dob = "10dec 1994"
+console.log(dob)
+
+// Q no-4
+
+var visitorName = "Rubaisha"
+var productTitle = "Bridal Dresses"
+var quantity = "5"
+var storeName = "Rusho Collection"
+
+var order = (visitorName +" "+productTitle +" "+ quantity +" "+ storeName)
+document.write(order)
